b09cb1636c9f219411c2b6d4677b3976ec69cd1c
liwb <liwb5@foxmail.com>
2017-11-03 11:34:04 +0800

    fix bug

--- code/seq2seq_translation_tutorial.py
+++ code/seq2seq_translation_tutorial.py
@@ -207,7 +207,7 @@ def readLangs(lang1, lang2, reverse=False):
     print("Reading lines...")
 
     # Read the file and split into lines
-    lines = open('data/%s-%s.txt' % (lang1, lang2), encoding='utf-8').\
+    lines = open('../data/%s-%s.txt' % (lang1, lang2), encoding='utf-8').\
         read().strip().split('\n')
 
     # Split every line into pairs and normalize
@@ -269,7 +269,7 @@ def prepareData(lang1, lang2, reverse=False):
     print("Read %s sentence pairs" % len(pairs))
     pairs = filterPairs(pairs)
     print("Trimmed to %s sentence pairs" % len(pairs))
-    print("Counting words..."
+    print("Counting words...")
     for pair in pairs:
         input_lang.addSentence(pair[0])
         output_lang.addSentence(pair[1])
