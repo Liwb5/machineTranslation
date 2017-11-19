dc48218ae41396512731a892b91b72fccb82bc5b
liwb <liwb5@foxmail.com>
2017-11-19 17:53:10 +0800

    rewrite dataProcess file to random the order of the train data

--- code/dataProcess.py
+++ code/dataProcess.py
@@ -83,13 +83,24 @@ def normalizeChinese(s):
 def readTrainLangs(lang1, lang2, reverse=True,fenci = False):
     print("Reading lines...")
 
+    en_lines = open('../data/train.%s'% lang2).read().strip().split('\n')
     zh_lines = open('../data/train.%s'% lang1).read().strip().split('\n')
-    #zh_lines = zh_lines[0:20]  #for test
-
+    data_length = len(zh_lines)
+    random_sort_sequence = random.sample(range(data_length),data_length)
+    
+    #random_sort_sequence = random_sort_sequence[0:100]##take 100 sentences for test .
+    
+    en_data_list = []
     zh_data_list = []
     if fenci:
         #jieba 分词
-        for line in zh_lines:
+        for index in random_sort_sequence:
+            #process english data-----------------------------
+            tmp = [normalizeString(s) for s in en_lines[index].split('\t')]
+            en_data_list.append(tmp)
+            
+            #process chinese data-----------------------------
+            line = zh_lines[index]
             seg_line = jieba.cut(line,cut_all=False)
             #dic = [seg for seg in seg_line]
             dic = ' '.join(seg_line)
@@ -99,7 +110,13 @@ def readTrainLangs(lang1, lang2, reverse=True,fenci = False):
                 tmp += val+' '
             zh_data_list.append(tmp)
     else: #用空格按字分开
-        for line in zh_lines:
+        for index in random_sort_sequence:
+            #process english data--------------------------------
+            tmp = [normalizeString(s) for s in en_lines[index].split('\t')]
+            en_data_list.append(tmp)
+            
+            #process chinese data---------------------------------
+            line = zh_lines[index]
             dic = ' '.join(line)
             tmp = ' '
             for char in dic.split(' '):
@@ -107,12 +124,16 @@ def readTrainLangs(lang1, lang2, reverse=True,fenci = False):
                 tmp += val+' '
             zh_data_list.append(tmp)
 
-    en_lines = open('../data/train.%s'% lang2).read().strip().split('\n')
-    #en_lines = en_lines[0:20]  #for test
+    
+    
     
     # Split every line into pairs and normalize
     #去掉一些标点符号
-    en_data_list = [[normalizeString(s) for s in l.split('\t')] for l in en_lines]
+    en_data_list = []
+    for index in random_sort_sequence:
+        tmp = [normalizeString(s) for s in en_lines[index].split('\t')]
+        en_data_list.append(tmp)
+        
     pairs = []
     if reverse:
         input_lang = Lang(lang2)
@@ -121,6 +142,7 @@ def readTrainLangs(lang1, lang2, reverse=True,fenci = False):
             input_lang.addSentence(en[0])
             output_lang.addSentence(zh)
             pairs.append([en[0].encode('utf-8'),zh.encode('gb2312')])
+            #pairs.append([en[0],zh])
     else:
         input_lang = Lang(lang1)
         output_lang = Lang(lang2)
@@ -161,4 +183,11 @@ if __name__=='__main__':
     h5.create_dataset('pairs',data=pairs,dtype = 'S400')
     h5.close()
     
+    #save data as pickle file
+    pickle_file = open('../data/test.pkl','wb')
+    pickle.dump(pairs,pickle_file)
+    pickle_file.close()
     
+    
+    p = open('../data/test.pkl','rb')
+    result = pickle.load(p)
